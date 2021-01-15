e48c39fd7a6e58d01c359a7ddc2094b538dfc20d
shoei_sato <46258448+satoss@users.noreply.github.com>
2021-01-15 14:18:18 +0900

    uiの修正

--- front/src/components/CalenderBoard/index.jsx
+++ front/src/components/CalenderBoard/index.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
-import { GridList } from '@material-ui/core'
+import { GridList, Typography } from '@material-ui/core'
 
 import { createCalender } from '../../services/calender'
 import CalenderElement from '../CalenderElement'
 import * as styles from './style.css'
 
 const calender = createCalender();
+const days = ["日", "月", "火", "水", "木", "金", "土"];
 
 const CalenderBoard = () => {
     return (
         <div className={styles.container}>
-            <GridList className={styles.grid} cols={8} spacing={0} cellHeight='auto'>
+            <GridList className={styles.grid} cols={7} spacing={0} cellHeight='auto'>
+            {days.map(d => (
+                <li key={d}>
+                    <Typography
+                        className={styles.days}
+                        color='textSecondary'
+                        align='center'
+                        variant='caption'
+                        component='div'
+                    >
+                        {d}
+                    </Typography>
+                </li>
+            ))}
             {calender.map((c, i) => (
                 <li key={c.toISOString()}>
-                    <CalenderElement>{c.format('D')}</CalenderElement>
+                    <CalenderElement day={c} />
                 </li>
             ))}
             </GridList>
--- front/src/components/CalenderElement/index.jsx
+++ front/src/components/CalenderElement/index.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import {Typography} from '@material-ui/core'
 
 import * as styles from './style.css'
 
-const CalenderElement = ({children}) => {
+const CalenderElement = ({day}) => {
+
+    const isFirstDay = day.date() === 1;
+    const format = isFirstDay ? 'M月D日' : 'D';
+
     return (
         <div className={styles.element}>
-            <div className={styles.date}>{children}</div>
+            <Typography
+                className={styles.date}
+                align='center'
+                variant='caption'
+                component='div'
+            >
+                {day.format(format)}
+            </Typography>
         </div>
     )
 }
+
 
 export default CalenderElement;
