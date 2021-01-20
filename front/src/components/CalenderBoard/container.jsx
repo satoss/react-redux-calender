ccfdee53fa9dc5f3b96b35646aa7ce6a7728bdb0
shoei_sato <46258448+satoss@users.noreply.github.com>
2021-01-20 14:37:31 +0900

    ダイアログの導入

--- front/src/components/CalenderBoard/container.jsx
+++ front/src/components/CalenderBoard/container.jsx
@@ -3,10 +3,25 @@ import { connect } from "react-redux";
 import { createCalender } from "../../services/calender";
 import CalenderBoard from "./presentation";
 
+import { addScheduleOpenDialog } from "../../redux/addSchedule/actions";
+
 const mapStateToProps = (state) => ({ calender: state.calender });
-const mergeProps = (stateProps) => ({
+
+const mapDispatchToProps = (dispatch) => ({
+  openAddScheduleDialog: () => {
+    dispatch(addScheduleOpenDialog());
+  },
+});
+
+const mergeProps = (stateProps, dispatchProps) => ({
+  ...stateProps,
+  ...dispatchProps,
   month: stateProps.calender,
   calender: createCalender(stateProps.calender),
 });
 
-export default connect(mapStateToProps, null, mergeProps)(CalenderBoard);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+  mergeProps
+)(CalenderBoard);
--- front/src/redux/rootReducer.js
+++ front/src/redux/rootReducer.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import calenderReducer from "./calender/reducer";
+import addScheduleReducer from "./addSchedule/reducer";
 
-const rootReducer = combineReducers({ calender: calenderReducer });
+const rootReducer = combineReducers({
+  calender: calenderReducer,
+  addSchedule: addScheduleReducer,
+});
 
 export default rootReducer;
